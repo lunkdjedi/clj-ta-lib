bbd227e50f785382c033f02a37d2b5397d0d3d2c
lunkdjedi <lunk.djedi@gmail.com>
2013-09-26 22:49:30 -0400

    Initial Commit of clj-ta-lib wrapper of TA-LIB

---
+++ src/main/clojure/clj_ta_lib/core.clj
@@ -0,0 +1,47 @@
+(ns clj-ta-lib.core
+   (:import [com.tictactec.ta.lib.meta CoreMetaData PriceHolder]
+            [com.tictactec.ta.lib MInteger]))
+
+(defn getFunc [func]
+  (CoreMetaData/getInstance func))
+
+
+(defn price-holder [flags open high low close volume]
+      (PriceHolder. flags 
+                    (double-array open)
+                    (double-array high)
+                    (double-array low)
+                    (double-array close)
+                    (double-array volume)
+                    (double-array (count open))))
+
+(defn acos [data-array]
+  (let [func (getFunc "acos")
+        input (double-array data-array)
+        size (count input)
+        begIndex (MInteger.)
+        outNbElements (MInteger.)
+        output (double-array size)]
+    (.setInputParamReal func 0 input)
+    (.setOutputParamReal func 0 output)
+    (.callFunc func 0 (- size 1) begIndex outNbElements)
+    (with-meta 
+      [(vec output)] 
+      {:begIndex (.value begIndex) :nbElements (.value outNbElements)})))
+
+(defn sma [window data-array]
+  (let [func (getFunc "sma")
+        input (double-array data-array)
+        size (count input)
+        begIndex (MInteger.)
+        outNbElements (MInteger.)
+        output (double-array size)]
+    (.setInputParamReal func 0 input)
+    (.setOutputParamReal func 0 output)
+    (.setOptInputParamInteger func 0 window)
+    (.callFunc func 0 (- size 1) begIndex outNbElements)
+    (with-meta 
+      [(vec output)] 
+      {:begIndex (.value begIndex) :nbElements (.value outNbElements)})))
+    
+  
