868be7327b2f75d99e9a1615803752f5cc00acb7
lunkdjedi <lunk.djedi@gmail.com>
2013-09-26 23:52:14 -0400

    Added Yahoo Data Fetching and PriceHolder Configuration
    Added Metadata for beginning index, number of elements and lookback
    
    Implemented sma and williams%r

--- src/main/clojure/clj_ta_lib/core.clj
+++ src/main/clojure/clj_ta_lib/core.clj
@@ -1,20 +1,26 @@
 (ns clj-ta-lib.core
    (:import [com.tictactec.ta.lib.meta CoreMetaData PriceHolder]
-            [com.tictactec.ta.lib MInteger]))
+            [com.tictactec.ta.lib MInteger])
+   (:use [clj-ta-lib.yahoo]))
 
 (defn getFunc [func]
   (CoreMetaData/getInstance func))
 
+(defn getFunctionInputFlags [func]
+  (.flags (.getInputParameterInfo (getFunc func) 0)))
 
-(defn price-holder [flags open high low close volume]
-      (PriceHolder. flags 
-                    (double-array open)
-                    (double-array high)
-                    (double-array low)
-                    (double-array close)
-                    (double-array volume)
-                    (double-array (count open))))
-
+(defn yahoo-price-holder 
+  [ticker function]
+	  (let [data (yahoo-vector ticker)
+         flags (getFunctionInputFlags function)]
+	    (PriceHolder. flags
+	                  (double-array (nth data 1))
+	                  (double-array (nth data 2))
+	                  (double-array (nth data 3))
+	                  (double-array (nth data 4))
+	                  (double-array (nth data 5))
+	                  (double-array (count (nth data 1))))))
+  
 (defn acos [data-array]
   (let [func (getFunc "acos")
         input (double-array data-array)
@@ -27,7 +33,7 @@
     (.callFunc func 0 (- size 1) begIndex outNbElements)
     (with-meta 
       [(vec output)] 
-      {:begIndex (.value begIndex) :nbElements (.value outNbElements)})))
+      {:begIndex (.value begIndex) :nbElements (.value outNbElements) :lookback (.getLookback func)})))
 
 (defn sma [window data-array]
   (let [func (getFunc "sma")
@@ -42,6 +48,22 @@
     (.callFunc func 0 (- size 1) begIndex outNbElements)
     (with-meta 
       [(vec output)] 
-      {:begIndex (.value begIndex) :nbElements (.value outNbElements)})))
+      {:begIndex (.value begIndex) :nbElements (.value outNbElements)  :lookback (.getLookback func)})))
+
+(defn willr [price-holder & {:keys [time-period] :or {time-period 14}}]
+  (let [func (getFunc "willr")
+        input price-holder
+        size (count (:c (bean input)))
+        begIndex (MInteger.)
+        outNbElements (MInteger.)
+        output (double-array size)]
+    (.setInputParamPrice func 0 input)
+    (.setOutputParamReal func 0 output)
+    (.setOptInputParamInteger func 0 time-period)
+    (.callFunc func 0 (- size 1) begIndex outNbElements)
+    (with-meta 
+      [(vec output)] 
+      {:begIndex (.value begIndex) :nbElements (.value outNbElements)  :lookback (.getLookback func)})))
+            
     
   
---
+++ src/main/clojure/clj_ta_lib/yahoo.clj
@@ -0,0 +1,53 @@
+(ns clj-ta-lib.yahoo
+  (:require [clj-time.core :as time]
+            [clj-time.format :as fmt])
+  (:import [com.tictactec.ta.lib.meta CoreMetaData PriceHolder])
+  (:use [clojure.core memoize]
+        [clojure.string :only (join split)]))
+  
+(def custom-formatter (fmt/formatter "yyyy-MM-dd"))
+
+(defn to-milliseconds-from-epoch [x]
+  (time/in-millis (time/interval (time/epoch) (fmt/parse custom-formatter x))))
+
+(defn to-year [x]
+  (time/year (fmt/parse custom-formatter x)))
+
+(defn days-ago-in-millis [d]
+  (time/in-millis (time/interval (time/epoch) (time/from-now (time/days (- 1 d))))))
+
+(defn- yahoo-raw-data [symbol]
+  (slurp (str "http://ichart.finance.yahoo.com/table.csv?s=" symbol)))
+
+(def yahoo-raw-data (ttl yahoo-raw-data :ttl/threshold (* 4 60 60 1000)))
+
+(defn yahoo-data 
+"
+Returns a sequence of vectors 
+where each vector represents daily open high low close and volume data
+([date open high low close volume adjclose])
+"
+[ticker]
+    (let [raw-data (yahoo-raw-data ticker)]
+    ;(let [raw-data (slurp (str "table-" ticker ".csv"))]
+    (map #(split % #",") 					 ; Split each line by comma
+        	(reverse                 ; Reverse Line order for proper date order
+            (drop 1             	 ; Drop first non-parsable header line
+            	(split raw-data #"\n")))))) ; Split the raw data by new line character
+
+
+(defn yahoo-vector [ticker]
+"
+Returns a vector of index-aligned vectors for stock history of provided ticker
+Dates maintain default string format while other values are converted to BigDecimal 
+[ [date] [open] [high] [low] [close] [volume] ]
+"
+    (let [column-data (apply map vector (yahoo-data ticker))]
+    [   (into [] (nth column-data 0))                   ;Date
+        (into [] (map read-string (nth column-data 1))) ;Open
+        (into [] (map read-string (nth column-data 2))) ;High
+        (into [] (map read-string (nth column-data 3))) ;Low
+        (into [] (map read-string (nth column-data 4))) ;Close
+        (into [] (map read-string (nth column-data 5))) ;Volume
+    ]))
+
