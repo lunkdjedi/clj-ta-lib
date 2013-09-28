576bf6f2e8b383e942e852a6d991a6fdc66de66e
lunkdjedi <lunk.djedi@gmail.com>
2013-09-28 15:26:27 -0400

    Helper function coming together, setting optional inputs from dynamic arity input.
    Helper function coming together, setting inputs from vector.

--- src/main/clojure/clj_ta_lib/core.clj
+++ src/main/clojure/clj_ta_lib/core.clj
@@ -1,7 +1,8 @@
 (ns clj-ta-lib.core
-   (:import [com.tictactec.ta.lib.meta CoreMetaData PriceHolder]
-            [com.tictactec.ta.lib.meta.annotation InputFlags]
-            [com.tictactec.ta.lib MInteger]))
+  (:import [com.tictactec.ta.lib.meta CoreMetaData PriceHolder]
+           [com.tictactec.ta.lib.meta.annotation InputFlags OptInputParameterType InputParameterType]
+           [com.tictactec.ta.lib MInteger]
+           [java.lang Exception]))
 
 (defn getFunc [func]
   (CoreMetaData/getInstance func))
@@ -84,7 +85,7 @@
     (.setOptInputParamInteger func 0 time-period)
     (.callFunc func 0 (- size 1) begIndex outNbElements)
     (with-meta 
-      [(vec output)] 
+      (vec output) 
       {:begIndex (.value begIndex) :nbElements (.value outNbElements)  :lookback (.getLookback func)})))
 
 (defn willr [price-holder & {:keys [time-period] :or {time-period 14}}]
@@ -103,4 +104,52 @@
       {:begIndex (.value begIndex) :nbElements (.value outNbElements)  :lookback (.getLookback func)})))
             
 
-  
+(defn helper [name input output & options]
+  (let [func (getFunc name)
+        nbOptInputs (-> func .getFuncInfo .nbOptInput)
+        nbInputs (-> func .getFuncInfo .nbInput)
+        nbOutputs (-> func .getFuncInfo .nbOutput)]
+
+    ;Set Options
+    (if (= (count options) nbOptInputs)
+      (doseq [i (range nbOptInputs)]
+        (let [pinfo (.getOptInputParameterInfo func i)]
+          (cond 
+           (or 
+             (= (-> pinfo .type) OptInputParameterType/TA_OptInput_RealRange)
+             (= (-> pinfo .type) OptInputParameterType/TA_OptInput_RealList))
+           (.setOptInputParamReal func i (str (nth options i)))
+           
+           (or 
+             (= (-> pinfo .type) OptInputParameterType/TA_OptInput_IntegerRange) 
+             (= (-> pinfo .type) OptInputParameterType/TA_OptInput_IntegerList))
+           (.setOptInputParamInteger func i (nth options i))
+           
+           :else
+           (throw (Exception. "InvalidArgument - Options"))
+           
+           )))
+      (throw (Exception. "Invalid number of options")))
+    
+    ;Set Inputs
+    (if (= (count input) nbInputs)
+	    (doseq [i (range nbInputs)]
+	      (let [pinfo (.getInputParameterInfo func i)]
+	        (cond
+	          (= (-> pinfo .type) InputParameterType/TA_Input_Price)
+	          (.setInputParamPrice func i (nth input i))
+	          
+	          (= (-> pinfo .type) InputParameterType/TA_Input_Real)
+	          (.setInputParamReal func i (nth input i))
+	          
+	          (= (-> pinfo .type) InputParameterType/TA_Input_Integer)
+	          (.setInputParamInteger func i (nth input i))
+	          
+	          :else 
+	          (throw (Exception. "Invalid Argument - Input"))
+	          
+	          )))
+     (throw (Exception. "Invalid number of inputs")))
+    
+    
+    ))
