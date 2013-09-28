088b7bd9e2fb1a479cd3dc078f6943db31065ae9
lunkdjedi <lunk.djedi@gmail.com>
2013-09-28 18:03:55 -0400

    Created a dynamic helper function that outputs vectors with metadata.

--- src/main/clojure/clj_ta_lib/core.clj
+++ src/main/clojure/clj_ta_lib/core.clj
@@ -1,6 +1,6 @@
 (ns clj-ta-lib.core
   (:import [com.tictactec.ta.lib.meta CoreMetaData PriceHolder]
-           [com.tictactec.ta.lib.meta.annotation InputFlags OptInputParameterType InputParameterType]
+           [com.tictactec.ta.lib.meta.annotation InputFlags OptInputParameterType InputParameterType OutputParameterType]
            [com.tictactec.ta.lib MInteger]
            [java.lang Exception]))
 
@@ -29,86 +29,21 @@
               InputFlags/TA_IN_PRICE_OPENINTEREST)
       flags)))
 
-(defn acos [data-array]
-  (let [func (getFunc "acos")
-        input (double-array data-array)
-        size (count input)
-        begIndex (MInteger.)
-        outNbElements (MInteger.)
-        output (double-array size)]
-    (.setInputParamReal func 0 input)
-    (.setOutputParamReal func 0 output)
-    (.callFunc func 0 (- size 1) begIndex outNbElements)
-    (with-meta 
-      [(vec output)] 
-      {:begIndex (.value begIndex) :nbElements (.value outNbElements) :lookback (.getLookback func)})))
-
-(defn sma [price-holder time-period & {:keys [column] :or {column :c}}]
-  (let [func (getFunc "sma")
-        input (column (bean price-holder))
-        size (count input)
-        begIndex (MInteger.)
-        outNbElements (MInteger.)
-        output (double-array size)]
-    (.setInputParamReal func 0 input)
-    (.setOutputParamReal func 0 output)
-    (.setOptInputParamInteger func 0 time-period)
-    (.callFunc func 0 (- size 1) begIndex outNbElements)
-    (with-meta 
-      [(vec output)] 
-      {:begIndex (.value begIndex) :nbElements (.value outNbElements)  :lookback (.getLookback func)})))
-
-(defn ema [price-holder time-period & {:keys [column] :or {column :c}}]
-  (let [func (getFunc "ema")
-        input (column (bean price-holder))
-        size (count input)
-        begIndex (MInteger.)
-        outNbElements (MInteger.)
-        output (double-array size)]
-    (.setInputParamReal func 0 input)
-    (.setOutputParamReal func 0 output)
-    (.setOptInputParamInteger func 0 time-period)
-    (.callFunc func 0 (- size 1) begIndex outNbElements)
-    (with-meta 
-      [(vec output)] 
-      {:begIndex (.value begIndex) :nbElements (.value outNbElements)  :lookback (.getLookback func)})))
-
-(defn rsi [price-holder time-period & {:keys [column] :or {column :c}}]
-  (let [func (getFunc "rsi")
-        input (column (bean price-holder))
-        size (count input)
-        begIndex (MInteger.)
-        outNbElements (MInteger.)
-        output (double-array size)]
-    (.setInputParamReal func 0 input)
-    (.setOutputParamReal func 0 output)
-    (.setOptInputParamInteger func 0 time-period)
-    (.callFunc func 0 (- size 1) begIndex outNbElements)
-    (with-meta 
-      (vec output) 
-      {:begIndex (.value begIndex) :nbElements (.value outNbElements)  :lookback (.getLookback func)})))
-
-(defn willr [price-holder & {:keys [time-period] :or {time-period 14}}]
-  (let [func (getFunc "willr")
-        input (addflags price-holder (getFunctionInputFlags func))
-        size (count (:c (bean input)))
-        begIndex (MInteger.)
-        outNbElements (MInteger.)
-        output (double-array size)]
-    (.setInputParamPrice func 0 input)
-    (.setOutputParamReal func 0 output)
-    (.setOptInputParamInteger func 0 time-period)
-    (.callFunc func 0 (- size 1) begIndex outNbElements)
-    (with-meta 
-      [(vec output)] 
-      {:begIndex (.value begIndex) :nbElements (.value outNbElements)  :lookback (.getLookback func)})))
-            
-
-(defn helper [name input output & options]
+(defn get-out-array [size ticks]
+  (into [] 
+        (for [i (range size)]
+          (double-array ticks))))
+    
+  
+(defn helper [name input & options]
   (let [func (getFunc name)
         nbOptInputs (-> func .getFuncInfo .nbOptInput)
         nbInputs (-> func .getFuncInfo .nbInput)
-        nbOutputs (-> func .getFuncInfo .nbOutput)]
+        nbOutputs (-> func .getFuncInfo .nbOutput)
+        begIndex (MInteger.)
+        outNbElements (MInteger.)
+        inputSize (atom nil)
+        output (atom nil)]
 
     ;Set Options
     (if (= (count options) nbOptInputs)
@@ -126,30 +61,53 @@
            (.setOptInputParamInteger func i (nth options i))
            
            :else
-           (throw (Exception. "InvalidArgument - Options"))
-           
-           )))
-      (throw (Exception. "Invalid number of options")))
+           (throw (Exception. "InvalidArgument - Options")))))
+      ((clj-ta-lib.util/print-function func)
+      (throw (Exception. "Invalid number of options"))))
     
     ;Set Inputs
     (if (= (count input) nbInputs)
-	    (doseq [i (range nbInputs)]
-	      (let [pinfo (.getInputParameterInfo func i)]
-	        (cond
-	          (= (-> pinfo .type) InputParameterType/TA_Input_Price)
-	          (.setInputParamPrice func i (nth input i))
-	          
-	          (= (-> pinfo .type) InputParameterType/TA_Input_Real)
-	          (.setInputParamReal func i (nth input i))
-	          
-	          (= (-> pinfo .type) InputParameterType/TA_Input_Integer)
-	          (.setInputParamInteger func i (nth input i))
-	          
-	          :else 
-	          (throw (Exception. "Invalid Argument - Input"))
-	          
-	          )))
-     (throw (Exception. "Invalid number of inputs")))
+			(doseq [i (range nbInputs)]
+			  (let [pinfo (.getInputParameterInfo func i)]
+			    (cond
+			      (= (-> pinfo .type) InputParameterType/TA_Input_Price)
+			      (.setInputParamPrice func i (addflags (nth input i) (getFunctionInputFlags func)))
+			      
+			      (= (-> pinfo .type) InputParameterType/TA_Input_Real)
+			      (.setInputParamReal func i (nth input i))
+			      
+			      (= (-> pinfo .type) InputParameterType/TA_Input_Integer)
+			      (.setInputParamInteger func i (nth input i)))))
+			((clj-ta-lib.util/print-function func) 
+			(throw (Exception. "Invalid number of inputs"))))
     
     
-    ))
+    ; At this point we need the size or number of ticks of the inputs
+    (let [pinfo (.getInputParameterInfo func 0)]
+	    (if (= (-> pinfo .type) InputParameterType/TA_Input_Price)
+	      (compare-and-set! inputSize nil (count (:c (bean (nth input 0)))))
+	      (compare-and-set! inputSize nil (count (nth input 0)))))
+    
+    ;Construct output arrays
+    (compare-and-set! output nil (get-out-array nbOutputs @inputSize))
+    
+    ;Set Output Paramters
+    (doseq [i (range nbOutputs)]
+      (let [pinfo (.getOutputParameterInfo func i)]
+        (cond
+          (= (-> pinfo .type) OutputParameterType/TA_Output_Real)
+          (.setOutputParamReal func i (nth @output i))
+          
+          (= (-> pinfo .type) OutputParameterType/TA_Output_Integer)
+          (.setOutputParamInteger func i (nth @output i))
+          
+          :else 
+          (throw (Exception. "Invalid Argument - Output")))))
+    
+    (.callFunc func 0 (- @inputSize 1) begIndex outNbElements)
+    
+    (with-meta
+      (into [] (map vec @output))
+      {:begIndex (.value begIndex) :nbElements (.value outNbElements)  :lookback (.getLookback func)}))
+        
+    )
--- src/main/clojure/clj_ta_lib/util.clj
+++ src/main/clojure/clj_ta_lib/util.clj
@@ -8,8 +8,10 @@
 (def group-service (proxy [TaGrpService] []
   (execute [group function-set] 
     (println (str "Group: " group))
-		  (doseq [function (seq function-set)]
-		    (println (str "   " (.name (.getFuncInfo function))))))))
+		  (doseq [ta-func (seq function-set)]
+		  (println (str "   " (-> ta-func .getFuncInfo .name)))
+      ;(print-function function)
+      ))))
 
 (defn print-groups []
   (CoreMetaData/forEachGrp group-service))
@@ -20,16 +22,16 @@
     ;PRINT INPUTS
       (doseq [i (range (-> function .getFuncInfo .nbInput))]
         (let [pinfo (.getInputParameterInfo function i)]
-          (println "  " (-> pinfo .paramName) (-> pinfo .type .name))))
+          (println "Input: " (-> pinfo .paramName) (-> pinfo .type .name))))
 
     ;PRINT OPTIONAL INPUTS
       (doseq [i (range (-> function .getFuncInfo .nbOptInput))]
         (let [pinfo (.getOptInputParameterInfo function i)]
-          (println "    " (-> pinfo .paramName) (-> pinfo .type .name))
+          (print "Option:" (-> pinfo .paramName))
           (cond 
             (= (-> pinfo .type) OptInputParameterType/TA_OptInput_RealRange) 
             (let [rrange (.getOptInputRealRange function i)] 
-              (println "     "
+              (println 
                 (str " min=" (.min rrange) 
 	                   " max=" (.max rrange) 
 	                   " precision=" (.precision rrange) 
@@ -37,25 +39,25 @@
                                                            
             (= (-> pinfo .type) OptInputParameterType/TA_OptInput_RealList) 
             (let [rlist (.getOptInputRealList function i)] 
-              (println "      " (map #(str %1 "(" %2 ")") (.string rlist) (.value rlist))))
+              (println "      " (map #(str %2 "-" %1) (.string rlist) (.value rlist))))
             
             (= (-> pinfo .type) OptInputParameterType/TA_OptInput_IntegerRange) 
             (let [irange (.getOptInputIntegerRange function i)] 
-              (println "     " 
+              (println 
                 (str " min=" (.min irange) 
                      " max=" (.max irange) 
                      " default=" (.defaultValue irange))))
 
             (= (-> pinfo .type) OptInputParameterType/TA_OptInput_IntegerList) 
             (let [ilist (.getOptInputIntegerList function i)]
-              (println "      " (map  #(str %1 "(" %2 ")") (.string ilist) (.value ilist))))
+              (println "" (map #(str %2 "-" %1) (.string ilist) (.value ilist))))
             )
           ))
     
     ;PRINT OUTPUTS
       (doseq [i (range (-> function .getFuncInfo .nbOutput))]
         (let [pinfo (.getOutputParameterInfo function i)]
-          (println "  " (-> pinfo .paramName) (-> pinfo .type .name)))))
+          (println "Output:" (-> pinfo .paramName) (-> pinfo .type .name)))))
 
   
 (def function-service (proxy [TaFuncService] []
